0b1793c2aec8aeeb50ef0188dfc23efde31b9339
siri kamkanampati <saisirikamkanampati@gmail.com>
2023-07-21 17:50:25 +0530

    changes made

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.json());
 //   });
 app.use(cors());
 app.use("/books",router);
-app.get('/', function(req, res){
-    res.render('app.js');
-  });
+// app.get('/', function(req, res){
+//     res.render('app.js');
+//   });
 // app.use(express.static(__dirname + '/public'));
 mongoose.connect("mongodb+srv://admin:itFz3lacFuJX2O7I@cluster0.7dgxr4j.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected to database"))
 .then(()=>{
