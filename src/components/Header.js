adaea9c386c539ee91b09918c98cb500722ae1fc
Kamkanampati Siri <93307903+Siri079@users.noreply.github.com>
2023-07-25 13:45:35 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                 </Typography></NavLink>
                 <Tabs sx={{ml:"auto"}} textColor='inherit' indicatorColor='primary' value={value} onChange={(e,val)=>setValue(val)}>
                     <Tab LinkComponent={NavLink} to="/add" label="Add New Book" />
-                    <Tab LinkComponent={NavLink} to="/books" label=" Books" />
+                    <Tab LinkComponent={NavLink} to="/books" label=" iBooks" />
                     <Tab LinkComponent={NavLink} to="/about" label="About Us" />
                 </Tabs>
                 </Toolbar>
